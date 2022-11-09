f3282e5ae9eb6973fa2be21c255c1efcbc3478fc
Asuka Langley Soryu <zh1610596998@gmail.com>
2022-11-09 16:43:50 +0800

    update input.c

--- IO与词法分析/input.c
+++ IO与词法分析/input.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //缓冲区设置
-#define MAXLINE 512
+#define MAXLINE 512 
 #define BUFSIZE 4096
 //指向缓冲区的双指针
 extern unsigned char *cp;
@@ -14,10 +14,44 @@ extern int lineno;      //行号
 //声明缓冲区
 static int bsize;
 static unsigned char buffer[BUFSIZE + 1 + MAXLINE + 1];
-
+//初始化缓冲区
 void inputInit(){
     
     limit=cp=&buffer[MAXLINE];
+    bsize=-1;
+    lineno=0;
+    file=NULL;
+    nextline();
 
-
+}
+//读取下一行
+void nextline(){
+    do
+    {
+        if(cp>=limit){
+            //填充缓冲区
+            // <refill buffer 80>
+            if(cp==limit){
+                return;
+            }
+        }else lineno++;
+        for(line=(char*)cp;*cp==' '||*cp=='\t';cp++){
+            ;
+        }
+    } while (*cp=='\n'&&cp==limit);
+    if(*cp=='#'){
+        resynch();
+        nextline();
+    }
+    
+}
+void fillbuf(){
+    if(bsize==0){
+        return;
+    }
+    if(cp>=limit){
+        cp=&buffer[MAXLINE+1];
+    }else{}
+    bsize=read(fileDescriptor,&buffer[MAXLINE+1],BUFSIZE);
+    
 }
